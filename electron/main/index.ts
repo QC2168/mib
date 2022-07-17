ba99775a1ebebaf79267e405b29f6c299b100a8a
QC2168 <huangxinhong2125@gmail.com>
2022-07-17 20:43:18 +0800

    refactor(main): react devtools

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,7 +1,10 @@
+import { log } from './../../src/utils/index';
 import { app, BrowserWindow, shell, ipcMain } from 'electron'
 import { release } from 'os'
 import { join } from 'path'
-
+import installExtension, {
+  REACT_DEVELOPER_TOOLS,REDUX_DEVTOOLS
+} from 'electron-devtools-installer';
 // Disable GPU Acceleration for Windows 7
 if (release().startsWith('6.1')) app.disableHardwareAcceleration()
 
@@ -57,9 +60,14 @@ async function createWindow() {
     if (url.startsWith('https:')) shell.openExternal(url)
     return { action: 'deny' }
   })
+
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow).then(()=>{
+  installExtension(REACT_DEVELOPER_TOOLS.id)
+  .then((name) => console.log(`Added Extension:  ${name}`))
+  .catch((err) => console.log('An error occurred: ', err));
+})
 
 app.on('window-all-closed', () => {
   win = null
