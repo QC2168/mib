c541064f1876e130032eba0046300cdfce70abf9
山田 <huangxinhong2125@gmail.com>
2022-08-27 12:14:13 +0800

    fix(build): 构建后白屏

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import 'uno.css';
 import '@/lib/css/index.less';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
 
