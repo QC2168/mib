3d9d6ec6416c1fa6ac7e57a2937605e2a45fc21d
QC2168 <huangxinhong2125@gmail.com>
2023-06-26 14:05:53 +0800

    fix(theme): unable to access preset theme value

--- state/themeState.ts
+++ state/themeState.ts
@@ -1,8 +1,7 @@
 import { atom } from 'recoil';
 import { Local } from '@/utils/storage';
-import { ThemeType } from '../src/lib/css/theme';
 
 export const themeModeState = atom({
   key: 'themeMode',
-  default: Local.get('themeMode') || ThemeType.LIGHT,
+  default: Local.get('themeMode') || 'light',
 });
